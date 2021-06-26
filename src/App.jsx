7a6d0488cb7084ae51adb9f6f5b5e4a93a7d2e0e
kiketion <kiketionje@gmail.com>
2021-06-26 00:55:35 -0500

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>Jackjack</code>!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kiketion"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          by JorgeenDev
         </a>
       </header>
     </div>
