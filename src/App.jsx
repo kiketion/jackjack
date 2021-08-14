faf2ee6145145122a7522c32fd12b9c1a9b587cd
Jorge Enrique Kike <59528563+kiketion@users.noreply.github.com>
2021-08-14 14:56:48 -0500

    codespaces test 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello everyone to <code>Jackjack</code>! enjoy!
+          Hello everyone, Welcome to <code>Jackjack</code>! enjoy!
         </p>
         <a
           className="App-link"
